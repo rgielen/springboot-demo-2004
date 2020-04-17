941961c74309e314a6a7a9453274f8c65b1d6ffb
Rene Gielen <rgielen@apache.org>
2020-04-17 09:08:12 +0200

    Create JPQL query method

--- src/main/java/com/example/springbootdemo/todo/TodoItemRepository.java
+++ src/main/java/com/example/springbootdemo/todo/TodoItemRepository.java
@@ -1,7 +1,9 @@
 package com.example.springbootdemo.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -12,5 +14,8 @@ import java.util.Optional;
 public interface TodoItemRepository extends JpaRepository<TodoItem, Integer> {
 
     Optional<TodoItem> findByTopic(String foo);
+
+    @Query("select t from TodoItem t where t.completed=true")
+    List<TodoItem> findCompleted();
 
 }
