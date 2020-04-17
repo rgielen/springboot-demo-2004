a1e54c42453bdcffbec21dd7364e2c504bb6d4ac
Rene Gielen <rgielen@apache.org>
2020-04-17 08:32:52 +0200

    Create empty Spring Boot application
    Created with https://start.spring.io

---
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.springbootdemo;
+
+import com.example.springbootdemo.todo.TodoItem;
+import com.example.springbootdemo.todo.TodoItemRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
