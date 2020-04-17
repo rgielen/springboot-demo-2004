b7e29e70e82c7ab2fa962e81416c10e269c11916
Rene Gielen <rgielen@apache.org>
2020-04-17 08:35:56 +0200

    Create simple hello endpoint with most simple controller setup

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -1,14 +1,7 @@
 package com.example.springbootdemo;
 
-import com.example.springbootdemo.todo.TodoItem;
-import com.example.springbootdemo.todo.TodoItemRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +12,16 @@ public class SpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
+
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @GetMapping
+    String sayHello() {
+        return "Hello";
+    }
 
 }
