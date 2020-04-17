d7ce12b868a99519a993643dbdfd7e89f69bf54f
Rene Gielen <rgielen@apache.org>
2020-04-17 09:20:04 +0200

    Optimize command line runner with lambda

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -1,10 +1,18 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.todo.TodoItem;
+import com.example.springbootdemo.todo.TodoItemRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
@@ -22,6 +30,22 @@ class HelloController {
     @GetMapping
     String sayHello() {
         return "Hello";
+    }
+
+}
+
+@Slf4j
+@Transactional
+@Component
+class Initializer {
+
+    @Bean
+    CommandLineRunner initDatabase(TodoItemRepository repository) {
+        return args -> {
+            repository.save(new TodoItem("Foo", false));
+            repository.save(new TodoItem("Bar", true));
+            log.info("Test beans created");
+        };
     }
 
 }
